21b3665726a071a9f932473d3121b0ae214542f3
Domingos Braganha <domingosbreganha9@gmail.com>
2023-06-29 00:12:07 +0100

    feat(migration): migartion para usuarios e telefone

--- database/migrations/2023_06_28_230438_create_users_table.php
+++ database/migrations/2023_06_28_230438_create_users_table.php
@@ -12,12 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
--- database/migrations/2023_06_28_230447_create_phones_table.php
+++ database/migrations/2023_06_28_230447_create_phones_table.php
@@ -11,10 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('phones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->string('phone_number');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
         });
     }
 
@@ -23,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('phones');
     }
 };
